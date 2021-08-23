c551b4c5ad805a24a3a137504870ae53c4b4a94b
Sander Meijer <sandertje1972@hotmail.com>
2021-08-23 10:16:33 +0200

    finished redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './App.css'
+import {useSelector, useDispatch} from "react-redux"
+import {increment, decrement} from "./actions"
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const disPatch = useDispatch()
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => disPatch(increment(2))}>+</button>
+      <button onClick={() => disPatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
     </div>
   );
 }
